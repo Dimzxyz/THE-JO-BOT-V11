cb2931557d76d0fa485de187ab2463ae8e78461a
Dimzxyz <119429825+Dimzxyz@users.noreply.github.com>
2022-12-05 19:08:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -17,14 +17,14 @@ global.APIKeys = {
 }
 
 
-global.ownername = 'THE JO BOT'
+global.ownername = '𝘿𝙞𝙢𝙯𝙮 𝙓𝘿'
 global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['6289514100625']
-global.premium = ['6289514100625']
-global.packname = 'The Jo Bot'
+global.owner = ['6283116775981']
+global.premium = ['6283116775981']
+global.packname = '𝘿𝙞𝙢𝙯𝙮 𝙓𝘿'
 global.author = 'WhatsApp Bot'
 global.wlcm = []
 global.sessionName = 'session'
